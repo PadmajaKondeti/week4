975a9540e893d9b677e937527d5212643abc0fcd
pkondeti <padma.kondeti@gmail.com>
2016-05-29 22:45:01 -0400

    added assets

--- assets/js/starwarMain.js
+++ assets/js/starwarMain.js
@@ -1,10 +1,31 @@
  
 $(document).ready(function() {
-        $("#move").click(function() {
-                $(".post").each(function() {
-                        var image = $(this).children("img");
-                        image.remove();
-                        $("#photoholder").append(image);
-                });
-        });                               
+
+   $(".character").on("click", function(){
+   		
+   		AddToEnemies(this);
+   		
+	});
+
+	function AddToEnemies(obj) {
+		debugger
+	var $this = $(obj).closest('div');
+	console.log($this);
+	var img = $this.find('div');
+	console.log(img);
+	$('#charEnemies').append(img);
+	}
+
+		/*$('.img-thumbnail').each(function() {
+			 var wrap = $(this);
+			 var hi = wrap.closest('.img-thumbnail').find('#charEnemies');
+			 //wrap.find('img').each(function() {
+			 var img = $(this);
+			 img.remove().appendTo(hi);
+			 //});
+		  //});
+
+   		$(this).css("float", "left"); */
+
+                       
 });
